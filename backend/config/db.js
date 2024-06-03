3e4f1359f4632f4939cf1e349d5a21e5f4f4b2ef
Pragati2324 <pragatikangude18@gmail.com>
2024-06-03 16:41:54 +0530

    env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 // db.js
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0/productTransactionsDB', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
-// server.js
+
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const connectDB = require('./config/db.js');
 const transactionRoutes = require('./routes/transactions.js');
 const statisticsRoutes = require('./routes/statistics.js');
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 connectDB();
@@ -17,5 +19,5 @@ app.use(morgan('dev'));
 app.use('/transactions', transactionRoutes);
 app.use('/statistics', statisticsRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
